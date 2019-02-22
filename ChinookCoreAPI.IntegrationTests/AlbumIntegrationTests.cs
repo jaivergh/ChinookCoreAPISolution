b770cd78509514fcf9f46ea43954687a72f62fa2
jcamilogh <jcamilogh@outlook.com>
2019-02-22 16:46:31 -0500

    Add project files.

---
+++ ChinookCoreAPI.IntegrationTests/AlbumIntegrationTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChinookCoreAPI.IntegrationTests
+{
+	public class AlbumIntegrationTests
+	{
+		private readonly HttpClient _client;
+		private readonly TestServer _server;
+
+		public AlbumIntegrationTests()
+		{
+			_server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+			_client = _server.CreateClient();
+		}
+
+		[Theory]
+		[InlineData("GET")]
+		public async Task TestAlbumAsync(string method)
+		{
+			//Arrange
+			var request = new HttpRequestMessage(new HttpMethod(method), "/api/Albums");
+
+			//Act
+			var response = await _client.SendAsync(request);
+
+			//Assert
+			response.EnsureSuccessStatusCode();
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+
+		[Theory]
+		[InlineData("GET", 1)]
+		public async Task TestOneAlbumAsync(string method, int? id = null)
+		{
+			//Arrage
+			var request = new HttpRequestMessage(new HttpMethod(method), $"/api/Albums/{id}");
+
+			//Act
+			var response = await _client.SendAsync(request);
+
+			//Assert
+			response.EnsureSuccessStatusCode();
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+	}
+}
